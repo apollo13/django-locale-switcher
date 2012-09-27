40cec5efe6c1e35104480a5bf1c8c6d06b3ba136
Florian Apolloner <florian@apolloner.eu>
2012-09-27 15:28:15 +0200

    Don't evaluate our context processor unless needed.

--- locale_switcher/context_processors.py
+++ locale_switcher/context_processors.py
@@ -3,17 +3,23 @@ from collections import namedtuple
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.utils import translation
+from django.utils.functional import lazy
+
 
 locale_info = namedtuple('locale_info', ['language_code', 'language_name', 'url', 'active'])
+
+
 def locale_switcher(request):
-    data = {'locale_switcher': []}
-    rm = request.resolver_match
-    # Translate the lanuages first:
-    lang_names = [translation.ugettext(i[1]) for i in settings.LANGUAGES]
-    for (lang_code, _), lang_name in zip(settings.LANGUAGES, lang_names):
-        with translation.override(lang_code):
-            url = reverse(rm.url_name, args=rm.args,
-                          kwargs=rm.kwargs, current_app=rm.app_name)
-            data['locale_switcher'].append(locale_info(lang_code, lang_name, url,
-                                           lang_code == request.LANGUAGE_CODE))
-    return data
+    def _get_locale_switcher():
+        data = []
+        rm = request.resolver_match
+        # Translate the lanuages first:
+        lang_names = [translation.ugettext(i[1]) for i in settings.LANGUAGES]
+        for (lang_code, _), lang_name in zip(settings.LANGUAGES, lang_names):
+            with translation.override(lang_code):
+                url = reverse(rm.url_name, args=rm.args,
+                              kwargs=rm.kwargs, current_app=rm.app_name)
+                data.append(locale_info(lang_code, lang_name, url,
+                                        lang_code == request.LANGUAGE_CODE))
+        return data
+    return {'locale_switcher': lazy(_get_locale_switcher, list)}
