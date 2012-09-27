5f9dadf6766d671cb565f19aac4ccb3680effac0
Florian Apolloner <florian@apolloner.eu>
2012-09-27 15:18:03 +0200

    Add current_app to the resolving and always use url_name.

--- locale_switcher/context_processors.py
+++ locale_switcher/context_processors.py
@@ -12,7 +12,8 @@ def locale_switcher(request):
     lang_names = [translation.ugettext(i[1]) for i in settings.LANGUAGES]
     for (lang_code, _), lang_name in zip(settings.LANGUAGES, lang_names):
         with translation.override(lang_code):
-            url = reverse(rm.url_name or rm.func, args=rm.args, kwargs=rm.kwargs)
+            url = reverse(rm.url_name, args=rm.args,
+                          kwargs=rm.kwargs, current_app=rm.app_name)
             data['locale_switcher'].append(locale_info(lang_code, lang_name, url,
                                            lang_code == request.LANGUAGE_CODE))
     return data
